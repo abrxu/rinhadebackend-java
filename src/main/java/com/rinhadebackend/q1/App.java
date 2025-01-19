82a27db9274b5dce89304b1051db81e5390b44c5
joao.silva <joao.silva@otisoftware.com.br>
2025-01-19 19:07:15 -0300

    ADD: added httphandler with some converting logic

--- src/main/java/com/rinhadebackend/q1/App.java
+++ src/main/java/com/rinhadebackend/q1/App.java
@@ -1,6 +1,21 @@
 package com.rinhadebackend.q1;
 
+import com.rinhadebackend.q1.controller.ClientesController;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(9092), 0);
+
+        server.createContext("/clientes", new ClientesController());
+
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server is running");
+
     }
 }
--- src/main/java/com/rinhadebackend/q1/controller/ClientesController.java
+++ src/main/java/com/rinhadebackend/q1/controller/ClientesController.java
@@ -1,9 +1,37 @@
 package com.rinhadebackend.q1.controller;
 
-public class ClientesController {
+import com.rinhadebackend.q1.service.ExtratoService;
+import com.rinhadebackend.q1.service.TransacoesService;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 
-    private final String baseUrl = "/clientes/";
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
-    public void get(String url, int idCliente, )
+public class ClientesController implements HttpHandler {
 
+    private TransacoesService transacoesService;
+    private ExtratoService extratoService;
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+        String httpMethod = exchange.getRequestMethod();
+        String rawRequestUrl = String.valueOf(exchange.getRequestURI());
+        String operation = rawRequestUrl.split("/")[3];
+        int idCliente = Integer.parseInt(rawRequestUrl.split("/")[2]);
+        String requestBody = convertRequestBodyIO(exchange.getRequestBody());
+        System.out.println(requestBody);
+
+    }
+
+    public String convertRequestBodyIO(InputStream requestBody) throws IOException {
+        ByteArrayOutputStream convertedRequestBody = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        for (int length; (length = requestBody.read(buffer)) != -1; ) {
+            convertedRequestBody.write(buffer, 0, length);
+        }
+        return convertedRequestBody.toString("UTF-8");
+    }
 }
